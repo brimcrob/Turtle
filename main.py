d08039ef6ea884cb28d06171738008c2c78cffe5
Brian McRoberts <brimcrob@gmail.com>
2022-01-16 17:18:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import turtle
+
+
+def stairs(size, nb):
+    # forward 30 up 30 use for loops
+    for i in range(0, nb):
+        t.forward(size)
+        t.left(90)
+        t.forward(size)
+        t.right(90)
+    #     decrementing steps
+    #     size = size - 10
+    #     size -= 10
+    t.forward(size)
+
+
+t = turtle.Turtle()
+
+
+def square(size, steps):
+    for i in range(0, steps):
+        t.forward(size)
+        t.left(90)
+        t.forward(size)
+
+
+square(100, 4)
+# stairs(60, 5)
+
+turtle.done()
